45038572cb1eb3701648370527f00e3b9d265243
rango527 <web.mobile497373@gmail.com>
2020-08-11 21:25:05 -0700

    responsive enlargement on large screens

--- components/bookmark.js
+++ components/bookmark.js
@@ -7,7 +7,7 @@ import Tags from './tags'
 import Title from './title'
 
 const cx = {
-  article: 'measure pb4 mb4 bb'
+  article: 'pb4 mb4 pb5-l mb5-l bb'
 }
 
 const Bookmark = ({ _id, cover, created, domain, excerpt, link, tags, title, type }) => {
